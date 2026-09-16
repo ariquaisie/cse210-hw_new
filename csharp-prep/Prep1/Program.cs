e14643702b29ae016c629d4dec5c9774baf88c0c
ariquaisie <ariellaqueenbay143@gmail.com>
2026-09-16 10:00:05 -0600

    initial code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
+        string firstname;
+        string lastname;
         Console.WriteLine("Hello Prep1 World!");
+        Console.Write("Please enter your first name ");
+        firstname=Console.ReadLine();
+        Console.Write("Please enter your last name ");
+        lastname=Console.ReadLine();
+
+        Console.WriteLine($"Your name is {firstname} {lastname}");
     }
 }
